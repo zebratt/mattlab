e0713788a5651120399affaedc4dfb4fd5c68160
xuejian.xu01 <xuejian.xu01@liulishuo.com>
2021-05-13 15:12:38 +0800

    update

--- webpack-mpa/src/pages/misc/index.tsx
+++ webpack-mpa/src/pages/misc/index.tsx
@@ -1,36 +1,66 @@
 //@ts-nocheck
 import { bootstrap } from '@/bootstrap';
-import React, { useEffect, useState, useMemo, useRef, useCallback } from 'react';
+import React, {
+  useEffect,
+  useState,
+  useMemo,
+  useRef,
+  useCallback,
+} from 'react';
+import 'intersection-observer'
 
-function useExpand(obj) {
-  const atom = useRef(obj.num * obj.num)
+import './style.less';
 
-  const abc = useCallback(() => {
-    console.log('use callback execute')
-  }, [atom])
-
-  const bbc = useMemo(() => {
-    console.log('use memo execute')
-  }, [abc])
-
-  return atom.current;
-}
+const images = [
+  'https://cdn.llscdn.com/misc/banner4-0ed59d5782546dfbf6b014afcfbdb574.png',
+  'https://cdn.llscdn.com/misc/banner3-cf86ac4fa7d78e124314b20fb89e6678.png',
+  'https://cdn.llscdn.com/misc/banner2-01d09ca8a63ed19d160c3f78c10cbbf9.png',
+  'https://sprout.llscdn.com/fbcb9911666f4e3ff112b90146f940e5.jpg',
+];
 
 function MiscPage() {
-  const [count, setCount] = useState(0);
-  const number = useExpand({num: 10});
+  const [offset, setOffset] = useState(0);
+
+  function move() {
+    setOffset((offset + 1) % 4);
+  }
+
+  useEffect(() => {
+    const targets = Array.from(document.querySelectorAll('.slide-item'));
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const map = entries.map((ele) => ({
+          id: ele.target.getAttribute('data-id'),
+          visible: ele.isIntersecting,
+        }));
+
+        console.log(map)
+      },
+      { threshold: 1 }
+    );
+
+    for (const ele of targets) {
+      observer.observe(ele);
+    }
+  }, []);
 
   return (
-    <div>
-      <p>count: {count}</p>
-      <p>number: {number}</p>
-      <button
-        onClick={async () => {
-          setCount(() => count + 1);
-        }}
+    <div className="misc-page">
+      <h1>misc page</h1>
+      <button onClick={move}>move</button>
+      <div
+        className="slides-wrapper"
+        style={{ transform: `translate3d(-${offset * 414}px, 0, 0)` }}
       >
-        increase
-      </button>
+        {images.map((ele, idx) => (
+          <img
+            data-id={`image-${idx + 1}`}
+            className="slide-item"
+            key={idx}
+            src={ele}
+          />
+        ))}
+      </div>
     </div>
   );
 }
--- webpack-mpa/src/pages/misc/slave.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function MiscPage() {
-  return <div>misc page2</div>;
-}
-
-export default MiscPage;
