7ce8d52d6609267315e5527f29bca149286d8ef3
xuejian.xu01 <xuejian.xu01@liulishuo.com>
2021-05-18 19:51:55 +0800

    update

--- webpack-mpa/src/pages/misc/index.tsx
+++ webpack-mpa/src/pages/misc/index.tsx
@@ -4,10 +4,12 @@ import React, { useState } from 'react';
 import Sub from './sub';
 
 function MiscPage() {
+  const [toggle, setToggle] = useState(false);
   return (
     <div>
       <h1>misc page</h1>
-      <Sub />
+      {toggle ? <Sub num={10} /> : <Sub num={100} />}
+      <button onClick={() => setToggle(!toggle)}>toggle</button>
     </div>
   );
 }
