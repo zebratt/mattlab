db951eee2c37492a21a0db7e46daa9bfb8fc0baf
xuejian.xu <xuejian.xu@mihoyo.com>
2022-04-13 14:57:57 +0800

    update

--- webpack-mpa/src/pages/misc/index.tsx
+++ webpack-mpa/src/pages/misc/index.tsx
@@ -1,12 +1,28 @@
 import { bootstrap } from '@/bootstrap';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Foo from './foo';
 
 function MiscPage() {
+  const [a, setA] = useState(null);
+
+  useEffect(() => {
+    if (a && a.name) {
+      console.log(a.name);
+    }
+  }, [a || null]);
+
   return (
     <div>
       <h1>misc page</h1>
-      <Foo />
+      <button
+        onClick={() => {
+          setA({
+            name: Date.now(),
+          });
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
