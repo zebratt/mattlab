32ff12a22c7cd5b1e53e785733eb19e09c57db3d
xuejian.xu <xuejian.xu@mihoyo.com>
2022-04-15 11:30:48 +0800

    update

--- umi-3.x/src/pages/index.tsx
+++ umi-3.x/src/pages/index.tsx
@@ -1,7 +1,13 @@
+import { Tooltip } from 'infosysD';
+import 'infosysD/lib/tooltip/style/index';
+import $ from './index.less';
+
 export default function IndexPage() {
   return (
-    <div>
-      <h1>home page</h1>
+    <div className={$.page}>
+      <Tooltip title="this is title blablabla" placement="topLeft">
+        <div className={$.title}>this is a title</div>
+      </Tooltip>
     </div>
   );
 }
