7e465661e61c9c5058f40f570f1ea03a08c54205
xuejian.xu <xuejian.xu@mihoyo.com>
2022-07-13 15:54:10 +0800

    update

--- umi-4/src/pages/index.tsx
+++ umi-4/src/pages/index.tsx
@@ -1,22 +1,7 @@
-import CustomRequest from 'info-request';
-
-const ctRequest = new CustomRequest();
-ctRequest.init({
-  prefix: 'http://localhost:8888',
-});
-const { request } = ctRequest;
-
 export default function HomePage() {
   return (
     <div>
       <h1>home page</h1>
-      <button onClick={() => {
-          request('/api/abc').then(() => {
-            console.log('sucess');
-          }).catch(() => {
-            console.log('error');
-          })
-      }}>click</button>
     </div>
   );
 }
