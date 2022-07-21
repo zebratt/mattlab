415dab5d01478fdea3291baae56809122d82ccfa
xuejian.xu <xuejian.xu@mihoyo.com>
2022-07-21 14:17:40 +0800

    update

--- umi-3.x/src/pages/index.tsx
+++ umi-3.x/src/pages/index.tsx
@@ -1,6 +1,14 @@
 import { useSelector } from 'react-redux';
 import { Link, RootState, store } from 'umi';
 import $ from './index.less';
+var timezone = require('dayjs/plugin/timezone');
+import dayjs from 'dayjs';
+import moment from 'moment';
+import { DatePicker } from 'antd';
+import 'antd/lib/date-picker/style/index';
+
+const time = dayjs('2022-07-14 18:21:15');
+const time2 = moment('2022-07-14 18:21:15');
 
 export default function IndexPage() {
   const { loading, count } = useSelector((state: RootState) => state.app);
@@ -20,6 +28,9 @@ export default function IndexPage() {
       <br />
       <br />
       <Link to={'/slave'}>slave app</Link>
+      {/* <DatePicker value={} showTime /> */}
+      <div>{time.toString()}</div>
+      <div>{time2.toString()}</div>
     </div>
   );
 }
